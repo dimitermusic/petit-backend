c4e0239b724a8744a585743eba92b3eec83031bd
kelliekumasaka <kelliek3@gmail.com>
2021-11-29 15:25:59 -0800

    route for google api

---
+++ controllers/api/googleRoute.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const router = express.Router();
+const tokenAuth = require("../../middleware/tokenAuth");
+const axios = require("axios")
+
+router.get("/", tokenAuth, (req, res) => {
+    return axios.get(`https://maps.googleapis.com/maps/api/place/textsearch/json?query=${req.body.name}%20in%20${req.body.city}&key=${process.env.API_KEY}`)
+      .then(function (response) {
+        res.json(response.data.results);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json({ err })
+      })
+});
+
+module.exports = router
--- controllers/api/index.js
+++ controllers/api/index.js
@@ -5,13 +5,14 @@ const placeRoutes = require("./placeRoutes");
 const reactionRoutes = require("./reactionRoutes");
 const userRoutes = require("./userRoutes");
 const voteRoutes = require("./voteRoutes");
-
+const googleRoutes = require("./googleRoute")
 
 router.use("/comments", commentRoutes);
 router.use("/places", placeRoutes);
 router.use("/reactions", reactionRoutes);
 router.use("/users", userRoutes);
 router.use("/votes", voteRoutes);
+router.use("/google", googleRoutes)
 router.get("/",(req,res)=>{
     res.send("Pet me 🐶")
 })
