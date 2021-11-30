4fa404b397bb2b88c2eccdc3d39c20f8f01cfb71
kelliekumasaka <kelliek3@gmail.com>
2021-11-30 01:11:27 -0800

    route changes for api

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -5,14 +5,14 @@ const placeRoutes = require("./placeRoutes");
 const reactionRoutes = require("./reactionRoutes");
 const userRoutes = require("./userRoutes");
 const voteRoutes = require("./voteRoutes");
-const googleRoutes = require("./googleRoute")
+const googleRoute = require("./googleRoute")
 
 router.use("/comments", commentRoutes);
 router.use("/places", placeRoutes);
 router.use("/reactions", reactionRoutes);
 router.use("/users", userRoutes);
 router.use("/votes", voteRoutes);
-router.use("/google", googleRoutes)
+router.use("/google", googleRoute)
 router.get("/",(req,res)=>{
     res.send("Pet me 🐶")
 })
